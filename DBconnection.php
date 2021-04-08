7bb2436b6ac2343733de20efc252aeeee015bb69
unknown <abodoom539@gmail.com>
2021-04-08 21:56:28 +0300

    added a database connections

---
+++ DBconnection.php
@@ -0,0 +1,13 @@
+<?php
+
+$database['host'] = 'localhost';
+        $database['username'] = 'root';
+        $database['userpass'] = '';
+        $database['name'] = 'users';
+
+        $GLOBALS['connection_link']  = mysqli_connect($database['host'], $database['username'],
+        $database['userpass'], $database['name']) or die(mysqli_connect_error());
+
+
+
+?>
--- sign.php
+++ sign.php
@@ -1,25 +1,62 @@
 <?php
 
 
-if (isset($_POST['submit'])){
-    // just testing POST and GET
-    $_fName = $_POST['firstName'];
-    $_lName = $_POST['lastName'];
-    $_userName = $_POST['userName'];
-    $_userEmail = $_POST['userEmail'];
-    $_password = $_POST['userPassword'];
-    $_confirmPass = $_POST['confirmPass'];
-
-    if($_password !== $_confirmPass){
-        echo "Passwords don't match";
+function checkUser(){
+    $GLOBALS['connection_link'];
+    $query = "SELECT * FROM users_info WHERE user_name='{$GLOBALS['userName']}'";
+    if ($result = mysqli_query($GLOBALS['connection_link'], $query)){
+        if(mysqli_num_rows($result) == '1'){
+            return true;
+        }
+        else return false;
     }
-    else
-    echo "<h3>Welcome!</h3>";
+    else return false;
+    }
 
+    function signUp(){
+        $query = "INSERT INTO users_info (first_name, last_name, user_email, user_name, user_pass) VALUES
+    ('{$GLOBALS['fName']}', '{$GLOBALS['lName']}', '{$GLOBALS['useremail']}', '{$GLOBALS['userName']}', '{$GLOBALS['password']}')";
+
+        if (mysqli_query($GLOBALS['connection_link'],$query) === true)
+        return true;
+        else
+        return false;
+    }
+
+
+if (isset($_POST['submit'])){
+
+    // just testing POST and GET
+  $GLOBALS['fName'] =  $_POST['firstName'];
+  $GLOBALS['lName'] = $_POST['lastName'];
+  $GLOBALS['userName'] = $_POST['userName'];
+  $GLOBALS['useremail'] = $_POST['userEmail'];
+  $GLOBALS['password'] = $_POST['userPassword'];
+  $GLOBALS['confrimPass'] = $_POST['confirmPass'];
+
+
+
+    if($GLOBALS['password'] !== $GLOBALS['confrimPass']){
+        echo "Passwords don't match try again!";
+    }
+    // stats here!
+    else
+        include 'DBconnection.php';
+
+        if(checkUser() === false){
+            if(signUp() === true){
+                echo "Account Registered";
+            }
+
+
+        }
+        else
+            echo "ERROR! Account already exists";
 }
 else
     echo "<h1>ERROR!</h1>";
 
 
+    include 'DBconnectionClose.php';
 
 ?>
